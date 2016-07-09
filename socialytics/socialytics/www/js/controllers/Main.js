1821e832aaf564a1042cd25b208cc0c2ad0a2610
AlexDewhirst <abdewhir@indiana.edu>
2016-07-09 11:22:18 -0700

    HTML/CSS

--- socialytics/socialytics/www/js/controllers/Main.js
+++ socialytics/socialytics/www/js/controllers/Main.js
@@ -1,22 +1,5 @@
-app.service("Socialytics", ["$http", "$log", Socialytics]);
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+'use strict'
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-
-  $ionicPlatform.refresh = function() {
-    alert("Button Pressed");
-  }
-})
+angular.module("homePage", []).controller('mainController', ['$scope', '$http', function($scope, $http) {
+  $scope.title = "Socialytics";
+}])
